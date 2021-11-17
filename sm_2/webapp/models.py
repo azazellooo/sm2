0264b03bd8d28e0873ec4775fb674f195dcb0a9e
karlygach <karygachkenenbaeva@gmail.com>
2021-11-17 14:38:10 +0600

    updated content identification from id to content_id

--- sm_2/webapp/models.py
+++ sm_2/webapp/models.py
@@ -2,15 +2,20 @@ from django.db import models
 
 
 class Content(models.Model):
-    project_name = models.CharField(max_length=500, null=False, blank=False, verbose_name='project name')
-    name_content = models.CharField(max_length=500, null=False, blank=False, verbose_name='content name')
-    resource = models.CharField(max_length=500, null=False, blank=False)
+    content_id = models.AutoField(primary_key=True)
+    project_name = models.CharField(max_length=500, null=False, blank=False)
+    name_content = models.CharField(max_length=500, null=False, blank=False)
+    resource = models.CharField(max_length=255, null=False, blank=False)
     id_format_content = models.PositiveIntegerField()
     url = models.CharField(max_length=500)
     success = models.IntegerField()
     success_data_update = models.IntegerField()
     acc_type = models.PositiveIntegerField()
     created_at = models.DateField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'content'
+        managed = False
 
 
 # Create your models here.
--- sm_2/webapp/views.py
+++ sm_2/webapp/views.py
@@ -17,7 +17,7 @@ class ContentListView(ListView, FormMixin):
     paginate_orphans = 10
 
     def get_queryset(self):
-        return Content.objects.using('metrics').all()
+        return Content.objects.using('metrics').all()[:10]
 
 
 def create_content(request, *args, **kwargs):
@@ -39,7 +39,7 @@ def create_content(request, *args, **kwargs):
 
 def delete_content(request, *args, **kwargs):
     if request.is_ajax and request.method == 'POST':
-        content = Content.objects.using('metrics').get(id=list(dict(request.POST).keys())[0])
+        content = Content.objects.using('metrics').get(content_id=list(dict(request.POST).keys())[0])
         content.delete()
         return JsonResponse({'message': f'content "{content.name_content}" is deleted successfully'}, status=200)
     return JsonResponse({"error": "something went wrong..."}, status=400, safe=False)
@@ -50,14 +50,14 @@ class ContentUpdateView(UpdateView):
     form_class = ContentForm
 
     def get(self, request, *args, **kwargs):
-        content = Content.objects.using('metrics').get(id=kwargs.get('pk'))
+        content = Content.objects.using('metrics').get(content_id=kwargs.get('pk'))
         form = ContentForm(instance=content)
         return HttpResponse(form.as_p())
 
     def post(self, request, *args, **kwargs):
         data = request.POST
         if request.is_ajax:
-            content = Content.objects.using('metrics').get(id=kwargs.get('pk'))
+            content = Content.objects.using('metrics').get(content_id=kwargs.get('pk'))
             content.project_name = data.get("project_name")
             content.name_content = data.get('name_content')
             content.resource = data.get('resource')
